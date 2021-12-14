2201201d10bc30fd69e8189176604c0bd059301c
Byron Linares <byron.hlr@gmail.com>
2021-12-14 17:40:24 -0600

    agregando logica para tablas covid, dags y archivo de conecciones

--- script/schema.sql
+++ script/schema.sql
@@ -1,3 +1,25 @@
+CREATE TABLE test.covid19_cases(
+    id int primary key auto_increment,
+    province	   varchar(256),
+    country	   varchar(256),
+    lat		   decimal(9,6),
+    longitud	   decimal(9,6),
+    date	   DATE,
+    count	   int,
+    doctype	   varchar(5)
+ );
+
+CREATE TABLE test.consolidate_covid19_cases(
+id int primary key auto_increment,
+province       varchar(256),
+country        varchar(256),
+lat            decimal(9,6),
+longitud       decimal(9,6),
+year_id int,
+month_id int,
+total decimal(20,2)
+);
+
 CREATE TABLE test.sales(
     id int primary key auto_increment,
     order_number int,
